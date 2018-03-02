99e399099cc794dd69fd9fd6d62b91e67e7c6dd7
Søren Vejrup Carlsen <svc@kb.dk>
2018-03-02 18:19:43 +0100

    Tring to generate the Mandelbrot with TensorFlow and java

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 JNI_DIR=jni
 VERSION=1.0-SNAPSHOT
+PROGRAM=dk.kb.TensorProgram
+#PROGRAM=dk.kb.MandelBrot
 if [ ! -d ${JNI_DIR} ]; then
  echo Missing directory: $JNI_DIR 
  echo Please run the download_jni.sh script first 
@@ -11,5 +13,10 @@ if [ ! -f ${JARFILE} ]; then
  echo Please run mvn clean install or mvn clean package first 
  exit 1;
 fi
+if [ ! -f ./strawberry.jpg ]; then
+wget https://docs.oracle.com/javase/tutorial/2d/images/examples/strawberry.jpg
+fi
+echo Running program $PROGRAM
+java -cp lib/libtensorflow-1.5.0.jar:target/intelligence-$VERSION.jar -Djava.library.path=jni $PROGRAM
 
-java -cp lib/libtensorflow-1.5.0.jar:target/intelligence-$VERSION.jar -Djava.library.path=jni dk.kb.TensorProgram
+
--- src/main/java/dk/kb/TensorProgram.java
+++ src/main/java/dk/kb/TensorProgram.java
@@ -11,24 +11,26 @@ import org.tensorflow.TensorFlow;
  */
 public class TensorProgram {
 
-  public static void main(String[] args) throws Exception {
-    try (Graph g = new Graph()) {
-      final String value = "Hello from " + TensorFlow.version();
+    public static void main(String[] args) throws Exception {
+        System.out.println("Starting program " + TensorProgram.class.getCanonicalName());
+        try (Graph g = new Graph()) {
+            final String value = "Hello from " + TensorFlow.version();
 
-      // Construct the computation graph with a single operation, a constant
-      //       // named "MyConst" with a value "value".
-                   try (Tensor t = Tensor.create(value.getBytes("UTF-8"))) {
-                           // The Java API doesn't yet include convenience functions for adding operations.
-                                   g.opBuilder("Const", "MyConst").setAttr("dtype", t.dataType()).setAttr("value", t).build();
-                                         }
-      
-                                               // Execute the "MyConst" operation in a Session.
-                                                     try (Session s = new Session(g);
-                                                                Tensor output = s.runner().fetch("MyConst").run().get(0)) {
-                                                                       System.out.println(new String(output.bytesValue(), "UTF-8"));
-                                                                              }
-                                                                                  }
-                                                                                    }
-                                                                 
-      
+            // Construct the computation graph with a single operation, a constant
+            // named "MyConst" with a value "value".
+            try (Tensor t = Tensor.create(value.getBytes("UTF-8"))) {
+                // The Java API doesn't yet include convenience functions for adding operations.
+                g.opBuilder("Const", "MyConst").setAttr("dtype", t.dataType()).setAttr("value", t).build();
+            }
+
+            // Execute the "MyConst" operation in a Session.
+            try (Session s = new Session(g);
+                    Tensor output = s.runner().fetch("MyConst").run().get(0)) {
+                System.out.println(new String(output.bytesValue(), "UTF-8"));
+            }
+        }
+        System.out.println("Finished program " + TensorProgram.class.getCanonicalName());
     }
+
+
+}
