8f46d0d10330fdccb79bdb4629719eef8cf56c34
Søren Vejrup Carlsen <svc@kb.dk>
2018-03-02 11:35:48 +0100

    updated the run.sh script to do some checks

--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
- java -cp libtensorflow-1.5.0.jar:target/intelligence-1.0-SNAPSHOT.jar -Djava.library.path=jni dk.kb.TensorProgram
+JNI_DIR=jni
+VERSION=1.0-SNAPSHOT
+if [ ! -d ${JNI_DIR} ]; then
+ echo Missing directory: $JNI_DIR 
+ echo Please run the download_jni.sh script first 
+ exit 1;
+fi 
+JARFILE=target/intelligence-$VERSION.jar
+if [ ! -f ${JARFILE} ]; then
+ echo Missing jarfile $JARFILE 
+ echo Please run mvn clean install or mvn clean package first 
+ exit 1;
+fi
+
+java -cp lib/libtensorflow-1.5.0.jar:target/intelligence-$VERSION.jar -Djava.library.path=jni dk.kb.TensorProgram
